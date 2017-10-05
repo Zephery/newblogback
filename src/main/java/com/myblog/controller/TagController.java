4fb963027f5b3f36cbd966962cedf9eab7577434
Zephery <1570631036@qq.com>
2017-10-05 21:37:48 +0800

    update

--- src/main/java/com/myblog/controller/TagController.java
+++ src/main/java/com/myblog/controller/TagController.java
@@ -3,16 +3,17 @@ package com.myblog.controller;
 import com.myblog.service.ITagService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Zephery on 2017/6/20.
  */
 @RestController
 public class TagController {
-    @Autowired
+    @Resource
     private ITagService tagService;
     private static final Logger logger= LoggerFactory.getLogger(TagController.class);
 
