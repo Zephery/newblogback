a7d7402950809b37c71cf47179c02eb442fafb50
Zephery <1570631036@qq.com>
2017-11-11 13:21:50 +0800

    update

--- src/main/java/com/myblog/shiro/ShiroConfiguration.java
+++ src/main/java/com/myblog/shiro/ShiroConfiguration.java
@@ -35,7 +35,7 @@ public class ShiroConfiguration {
         //Shiro的核心安全接口,这个属性是必须的
         shiroFilterFactoryBean.setSecurityManager(securityManager);
         //要求登录时的链接(可根据项目的URL进行替换),非必须的属性,默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        shiroFilterFactoryBean.setLoginUrl("/login.jsp");
+        shiroFilterFactoryBean.setLoginUrl("/login.html");
         //登录成功后要跳转的连接,逻辑也可以自定义，例如返回上次请求的页面
         shiroFilterFactoryBean.setSuccessUrl("/admin/index");
         //用户访问未对其授权的资源时,所显示的连接
@@ -49,7 +49,7 @@ public class ShiroConfiguration {
          * anon：它对应的过滤器里面是空的,什么都没做,这里.do和.jsp后面的*表示参数,比方说login.jsp?main这种
          * authc：该过滤器下的页面必须验证后才能访问,它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter
          */
-        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
 
